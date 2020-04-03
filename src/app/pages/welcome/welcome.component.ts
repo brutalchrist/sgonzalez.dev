30da3a096350d7d9b7150c6a67aac1dbdf4033c4
Sebastián González <sgonzalez@debianchile.cl>
2020-04-03 20:31:26 -0300

    delete Audio

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -14,9 +14,5 @@ export class WelcomeComponent implements OnInit {
 
   handleMicetown = () => {
     console.log('Beware of the mice!');
-    const audio = new Audio();
-    audio.src = '../../assets/audio/micetown.m4a';
-    audio.load();
-    audio.play();
   }
 }
